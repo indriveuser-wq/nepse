c4e815b00b1392c42b155857140b41f1bac8c2dc
indriveuser-wq <indriveuser@gmail.com>
2026-04-19 18:20:24 +0000

    use scrapper

--- backend/app.py
+++ backend/app.py
@@ -3,23 +3,27 @@ import requests
 
 app = FastAPI()
 
-BASE_URL = "https://nepsealpha.com/api/v1/market-summary"
+SCRAPER_API_KEY = "7a75bde7c78f8035db7ddf321bc67f98"
+
+TARGET_URL = "https://nepsealpha.com/api/v1/market-summary"
+
 
 @app.get("/")
 def home():
     return {"status": "ok"}
 
+
 @app.get("/prices")
 def get_prices():
     try:
-        res = requests.get(BASE_URL, timeout=10)
+        url = f"http://api.scraperapi.com?api_key={SCRAPER_API_KEY}&url={TARGET_URL}"
+
+        res = requests.get(url, timeout=15)
 
         print("Status:", res.status_code)
 
         if res.status_code == 200:
             data = res.json()
-
-            # 🔥 FIX: correct data path
             return data.get("data", [])
 
     except Exception as e:
